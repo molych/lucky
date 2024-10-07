ffa88a1b4c9f4ee2fab4737e3afe3156709a76dc
Oleksandr Mochalov <molych69@gmail.com>
2024-10-07 13:33:11 +0300

    Add detailed win amount tests and update .eslintignore
    Enhanced the WinAmountTest to cover multiple scenarios based on ranges of random numbers. Also fixed formatting issues in .eslintignore for consistency.

--- tests/Feature/WinAmountTest.php
+++ tests/Feature/WinAmountTest.php
@@ -1,7 +1,31 @@
 <?php
 
-use App\Services\ResultCheckerService;
+use App\Services\WinningStrategyFactory;
 
-it('win amount test', function () {
+it('calculates the win amount correctly for random number greater than 900', function () {
+    $strategyFactory = new WinningStrategyFactory();
+    $strategy = $strategyFactory::getStrategy(950);
+    $winAmount = $strategy->calculateWinAmount(950);
+    expect($winAmount)->toBe((int) floor(950 * 0.7));
+});
 
+it('calculates the win amount correctly for random number greater than 600 but less than or equal 900', function () {
+    $strategyFactory = new WinningStrategyFactory();
+    $strategy = $strategyFactory::getStrategy(700);
+    $winAmount = $strategy->calculateWinAmount(700);
+    expect($winAmount)->toBe((int) floor(700 * 0.5));
+});
+
+it('calculates the win amount correctly for random number greater than 300 but less than or equal 600', function () {
+    $strategyFactory = new WinningStrategyFactory();
+    $strategy = $strategyFactory::getStrategy(400);
+    $winAmount = $strategy->calculateWinAmount(400);
+    expect($winAmount)->toBe((int) floor(400 * 0.3));
+});
+
+it('calculates the win amount correctly for random number less than or equal 300', function () {
+    $strategyFactory = new WinningStrategyFactory();
+    $strategy = $strategyFactory::getStrategy(200);
+    $winAmount = $strategy->calculateWinAmount(200);
+    expect($winAmount)->toBe((int) floor(200 * 0.1));
 });
